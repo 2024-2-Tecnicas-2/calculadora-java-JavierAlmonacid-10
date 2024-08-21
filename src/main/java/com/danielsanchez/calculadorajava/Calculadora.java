a47bd45c00abbc4ae7719e3350f378e82cc0da05
elmatador <elmatador@yahoo.com>
2024-08-21 18:48:04 -0500

    Solución

--- src/main/java/com/danielsanchez/calculadorajava/Calculadora.java
+++ src/main/java/com/danielsanchez/calculadorajava/Calculadora.java
@@ -28,6 +28,14 @@ public class Calculadora {
                 break;
             
             // TODO: DEBES CREAR AQUÍ LOS CASES PARA LA OPERACIÓN DE LA POTENCIA Y EL MÓDULO.
+                case '^' :
+                // Operación división:
+                respuesta = Math.pow(numero1, numero2);
+                break;
+                case '%' :
+                // Operación división:
+                respuesta =  numero1 % numero2;
+                break;
                 
             default:
                 throw new IllegalArgumentException("Operación inválida.");
